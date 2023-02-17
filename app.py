b6ad143986fed117582d23d82937b8bb9eedf200
allen3680 <allen3680@gmail.com>
2023-02-17 17:42:07 +0800

    功能都完成剩下分頁跟搜尋

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request # 導入模組Flask
+from flask import Flask, render_template, request, redirect # 導入模組Flask
 from datetime import datetime
 app = Flask(__name__)  # 呼叫Flask的固定用法
 
@@ -19,8 +19,6 @@ def customerTable():
     cursor.execute(sqlStuff)
     customerInfo=cursor.fetchall()
 
-    print(customerInfo)
-
     return render_template('table.html',customerInfo=customerInfo)
 
 @app.route("/create")
@@ -33,8 +31,6 @@ def edit(serial_no):
     sqlStuff = "select * from Customer where serial_no = " + serial_no
     cursor.execute(sqlStuff)
     customerInfo=cursor.fetchone()
-
-    print(customerInfo)
 
     return render_template('index.html',customerInfo=customerInfo)
 
@@ -57,11 +53,15 @@ def submit():
     remark = request.values['remark']
 
     sqlStuff = "INSERT INTO Customer (id, name, checkin, checkout, days, gender, room_no, phone, address, remark) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+
+    if (request.values['serial_no'] != ""):
+        sqlStuff = "UPDATE Customer SET id=%s, name=%s, checkin=%s, checkout=%s, days=%s, gender=%s, room_no=%s, phone=%s, address=%s, remark=%s WHERE serial_no="+request.values['serial_no']
+
     records = (idno, name, checkin, checkout, days, gender, room_no, phone, address, remark)
     cursor.execute(sqlStuff, records)
     mydb.commit()
 
-    return customerTable();
+    return redirect("/");
 
 if __name__ == "__main__":  # 如果主程式執行
     app.run()  # 啟動伺服器
